0db2fae33ac0126e9031cd5bb019d3944b9133e4
Jith Stephen <jithstephen@Jiths-MacBook-Air.local>
2026-01-18 23:56:49 +0530

    bug fixes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <--- ADD THIS LINE
   reactStrictMode: true,
   images: {
-    unoptimized:true,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
-        pathname: ' **',
+        pathname: '/**', // Fixed: added leading slash
       },
     ],
   },
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
 }
 
 module.exports = nextConfig
